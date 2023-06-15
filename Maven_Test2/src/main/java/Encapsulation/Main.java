23ba98d433b43a3d35fb3cff6077a339a8021e77
SREELAKSHMI <SREELAKSHMI@LAPTOP-QCSBJUUB>
2023-06-15 10:56:18 +0530

    Encapsulation method

---
+++ Maven_Test2/src/main/java/Encapsulation/Main.java
@@ -0,0 +1,12 @@
+package Encapsulation;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Encap o = new Encap();
+o.setName("SREE", 10);
+o.getName();
+	}
+
+}
